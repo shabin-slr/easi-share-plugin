8c199f33d0fb7f1b797159368b0714fafb129666
shabin.muhammed <shabin@gravitum.com>
2019-03-16 21:13:38 +0400

    basic app

--- ng-app/app.js
+++ ng-app/app.js
@@ -4,11 +4,11 @@ angular.module('easishare-plugin',['ngRoute']);
 angular.module('easishare-plugin').config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl: "/ng-app/pages/auth/auth.html",
+        templateUrl: "./ng-app/pages/auth/auth.html",
         controller: "authController"
     })
     .when("/files", {
-        templateUrl: "/ng-app/pages/files/files.html",
+        templateUrl: "./ng-app/pages/files/files.html",
         controller: "filesController"
     })
 });
